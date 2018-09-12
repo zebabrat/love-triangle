27d34797e016a61f26fa230ebec173925f20efb7
Andrey <36641918+zebabrat@users.noreply.github.com>
2018-09-13 01:46:31 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(testArray = []) {
-  var count = 0
+  let count = 0
   testArray.map((el, index) => {
     if (index === testArray[testArray[testArray[index] - 1] - 1] - 1 && index + 1 !== testArray[index]) {
       count++
